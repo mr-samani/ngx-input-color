80674a6711d202dea6e49e7cacb277343e249ac8
mr.samani <mr.samani1368@gmail.com>
2025-07-08 08:32:59 +0330

    change demo routes

---
+++ projects/demo/src/app/demo-page/demo-page.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgxInputColorModule } from 'ngx-input-color';
+
+@Component({
+  selector: 'app-demo-page',
+  templateUrl: './demo-page.component.html',
+  styleUrls: ['./demo-page.component.scss'],
+  imports: [CommonModule, FormsModule, NgxInputColorModule],
+  standalone: true,
+})
+export class DemoPageComponent implements OnInit {
+  color = 'blueviolet';
+  constructor() {}
+
+  ngOnInit() {}
+}
