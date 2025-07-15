12c5cd4293be15fa21f1fba36f50c1a85b9d3c68
mr.samani <mr.samani1368@gmail.com>
2025-07-15 11:13:35 +0330

    change path of story book

--- projects/storybook/src/stories/button.stories.ts
+++ projects/storybook/src/stories/button.stories.ts
@@ -1,12 +1,8 @@
-import { applicationConfig, moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
-import { fn } from 'storybook/test';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 
-import { ButtonComponent } from './button.component';
-import { NgxInputColorComponent, NgxInputColorDirective } from '../public-api';
-import { NgxInputColorModule } from '../ngx-input-color.module';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
-import { EnumToArrayPipe } from '../pipes/enum-to-array.pipe';
+import { NgxInputColorComponent } from '../../../ngx-input-color/src/public-api';
+import { NgxInputColorModule } from '../../../ngx-input-color/src/ngx-input-color.module';
+import { EnumToArrayPipe } from '../../../ngx-input-color/src/pipes/enum-to-array.pipe';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
