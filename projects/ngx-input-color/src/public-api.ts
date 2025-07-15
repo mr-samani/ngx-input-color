c3ef9dc7f4fb366f11afc276c90d25713a649009
mr.samani <mr.samani1368@gmail.com>
2025-07-15 09:17:54 +0330

    add ngx-input-gradient.directive to public api

--- projects/ngx-input-color/src/public-api.ts
+++ projects/ngx-input-color/src/public-api.ts
@@ -8,5 +8,6 @@ export * from './lib/ngx-input-gradient/ngx-input-gradient.component';
 export * from './ngx-input-color.module';
 
 export * from './directives/ngx-input-color.directive';
+export * from './directives/ngx-input-gradient.directive';
 
 export * from './models/IPosition';
