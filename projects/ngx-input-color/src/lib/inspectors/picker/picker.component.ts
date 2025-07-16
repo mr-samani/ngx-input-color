04eb5fd37aea3cf9e1928bd0ca3d0d23efdfae12
mr.samani <mr.samani1368@gmail.com>
2025-07-16 08:58:35 +0330

    fix bug saturation in color picker

--- projects/ngx-input-color/src/lib/inspectors/picker/picker.component.ts
+++ projects/ngx-input-color/src/lib/inspectors/picker/picker.component.ts
@@ -23,7 +23,8 @@ export class PickerComponent implements OnInit {
     this.hue = shva.h;
     this.board = { x: shva.s, y: 100 - shva.v };
     this.alpha = shva.a ?? 1;
-    this.baseColor = c.toHexString(false);
+    const pureColor = new NgxColor({ h: this.hue, s: 100, v: 100, a: 1 });
+    this.baseColor = pureColor.toHexString(false);
   }
   @Output() colorChange = new EventEmitter<NgxColor | undefined>();
 
@@ -35,7 +36,8 @@ export class PickerComponent implements OnInit {
     try {
       const hsva: HSVA = { h: this.hue, s: this.board.x, v: 100 - this.board.y, a: this.alpha };
       const color = new NgxColor(hsva);
-      this.baseColor = color.toHexString(false);
+      const pureColor = new NgxColor({ h: this.hue, s: 100, v: 100, a: 1 });
+      this.baseColor = pureColor.toHexString(false);
       if (color.equals(this.inputColor) == false) {
         this.inputColor = color;
         this.colorChange.emit(color);
